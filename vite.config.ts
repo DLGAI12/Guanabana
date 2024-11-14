b3a1e864ba54aed04fd39c873a23294ebba172d9
DLGAI12 <adrian.deleon0252@alumnos.udg.mx>
2024-11-14 00:38:36 -0600

    A

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-  port: process.env.PORT ||10000,  // Usa el puerto proporcionado por el entorno o 3000 como valor por defecto
+  port: Number(process.env.PORT) ||10000,  // Usa el puerto proporcionado por el entorno o 3000 como valor por defecto
 }
 })
 
