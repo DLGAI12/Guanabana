d58f14ced9e242510884caf376ad1a1aa1ab6f37
DLGAI12 <adrian.deleon0252@alumnos.udg.mx>
2024-11-13 22:52:26 -0600

    Intento 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Inicio from './Inicio';
 import Login from './Login';
 import Catalogo from './Catalogo';
@@ -9,6 +9,11 @@ import RegistrarPedido from './RegistrarPedido';
 import EmpresaDashboard from './CatalogoEmpresa';
 
 const App: React.FC = () => {
+  const location = useLocation();
+
+  // Si el idUsuario se pasa como estado, lo recuperamos desde location
+  const { idUsuario } = location.state || {}; // Asegúrate de que 'idUsuario' esté en el state
+
   return (
     <Router>
       <Routes>
@@ -17,7 +22,10 @@ const App: React.FC = () => {
         <Route path="/consulta-reparto" element={<ConsultaReparto />} />
         <Route path="/catalogo" element={<Catalogo />} />
         <Route path="/registro" element={<Registro />} />
-        <Route path="/registrar-pedido" element={<RegistrarPedido />} />
+        <Route 
+          path="/registrar-pedido" 
+          element={idUsuario ? <RegistrarPedido idUsuario={idUsuario} /> : <div>No se encontró el idUsuario.</div>} 
+        />
         <Route path="/empresa-dashboard" element={<EmpresaDashboard />} />
       </Routes>
     </Router>
