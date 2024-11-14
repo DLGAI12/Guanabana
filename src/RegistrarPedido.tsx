52a00a15d7fb50cb645c49197eea5c8a21917ca9
DLGAI12 <adrian.deleon0252@alumnos.udg.mx>
2024-11-13 23:12:59 -0600

    Intento 11

--- src/RegistrarPedido.tsx
+++ src/RegistrarPedido.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-import '/registrarPedido.css';
+import './registrarPedido.css';
 interface RegistrarPedidoProps {
   idUsuario: number; // O el tipo correspondiente, como `string`, si es el caso
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    // Asegúrate de que Vite procese el CSS correctamente
-    preprocessorOptions: {
-      css: {
-        // Aquí puedes agregar configuraciones si usas preprocesadores como SCSS, etc.
-      },
-    },
-  },
 })
